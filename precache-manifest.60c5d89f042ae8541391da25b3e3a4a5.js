e8da573109714ce1959f600bc06b11e7e75f7e48
Caleb Kellicutt <caleb.Kellicutt@eagledream.com>
2020-01-17 15:55:56 -0500

    Updates

---
+++ precache-manifest.60c5d89f042ae8541391da25b3e3a4a5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22ae2556586012343f9d3388ffee312",
+    "url": "/stock/index.html"
+  },
+  {
+    "revision": "69ed76294fbd4489bbe7",
+    "url": "/stock/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ab6e9cf52ed203a6ad26",
+    "url": "/stock/static/js/2.ce0a6e12.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stock/static/js/2.ce0a6e12.chunk.js.LICENSE"
+  },
+  {
+    "revision": "69ed76294fbd4489bbe7",
+    "url": "/stock/static/js/main.71f80cdd.chunk.js"
+  },
+  {
+    "revision": "4a69d63221fce0968a5f",
+    "url": "/stock/static/js/runtime-main.927b3c87.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/stock/static/media/logo.5d5d9eef.svg"
+  }
+]);
